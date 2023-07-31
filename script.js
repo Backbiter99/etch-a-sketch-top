1addf0047b84e202fa6ba68aee82f38e0792b87b
Backbiter99 <prajwaljha@gmail.com>
2023-07-31 19:47:14 +0530

    added button to change dimensions of the grid The size of the grid does not change upon changing the dimensions of the grid. Also added some styles for a better visual experience.

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const container = document.getElementById("container");
+const button = document.getElementById('button');
+
 
 function makeGrid(size) {
+  const widthPercent = 100/size;
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement("div");
     square.classList.add("square-div");
+    square.style.width = widthPercent + "%";
+    square.style.paddingBottom = widthPercent + "%";
     container.appendChild(square);
 
     square.addEventListener("mouseover", (e) => {
@@ -12,4 +17,24 @@ function makeGrid(size) {
   }
 }
 
+function deleteGrid(){
+  while(container.childElementCount){
+    container.removeChild(container.firstChild);
+  }
+};
+
+function changeGrid(gridValue){
+  deleteGrid();
+  makeGrid(gridValue);
+}
+
 makeGrid(16);
+
+
+button.addEventListener('click', (e)=>{
+  let gridValue = +prompt("Enter the size of the grid");
+  if(gridValue > 0 && gridValue <= 100){
+    changeGrid(gridValue);
+  }
+  else alert("Invalid Input!")
+})
